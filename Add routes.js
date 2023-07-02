0217f659d3699d15451976e79e31b5dcbcc6d5a9
Kaushal99999 <96714323+Kaushal99999@users.noreply.github.com>
2023-07-02 14:20:25 +0530

    Update Add routes.js

--- Add routes.js
+++ Add routes.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const extractedValues = [];
 const filename = 'data.txt';
-// Step 1: Set up Mongoose connection
-mongoose.connect('mongodb+srv://root:tiger@cluster0.hmlmk6v.mongodb.net/cluster0?retryWrites=true&w=majority', {
+
+// replace with your db connection url
+
+mongoose.connect('your database connection', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -24,11 +26,9 @@ const costMatrixElementSchema = new mongoose.Schema({
   });
   
 
-// Step 3: Create a Mongoose model
-const CostMatrixElement = mongoose.model('CostMatrixElement', costMatrixElementSchema);
-// Step 4: Create a new instance of the model
 
- // Replace with your actual file name or path
+const CostMatrixElement = mongoose.model('CostMatrixElement', costMatrixElementSchema);
+
 
 fs.readFile(filename, 'utf8', (err, data) => {
   if (err) {
@@ -62,17 +62,16 @@ fs.readFile(filename, 'utf8', (err, data) => {
       Mark2:mark2,
       route:latLngPairs}
 
-    // Process the data for each record as per your requirements
     extractedValues.push(extracted);
   });
   CostMatrixElement.insertMany(extractedValues)
         .then(() => {
           
           console.log('Inserted successfully');
-           // Resolve the promise with the extracted values
+      
         })
         .catch((error) => {
-          console.log(error);// Reject the promise with the error
+          console.log(error);
         });
  
 
